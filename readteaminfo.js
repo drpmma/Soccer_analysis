a3582589efe1722a4e8ec5ad5b53d1f7927c08c0
Wu Jiang <wujiang5521@qq.com>
2017-07-11 23:26:20 +0800

    teaminfo

--- readteaminfo.js
+++ readteaminfo.js
@@ -1,9 +1,18 @@
 /**
  * Created by WuJiang on 2017/7/8.
  */
-
+var time = 2700;
 var field = document.getElementById("game_title_field").getBoundingClientRect();
 var field_w = field.width, field_h = field.height;
+var teamchoose = 0;
+repaint_player_svg();
+
+function setteam(t)
+{
+    teamchoose = t;
+    console.log(teamchoose);
+    repaint_player_svg();
+}
 
 d3.csv("gameinfo.csv", function(error,csvdata){
     if(error) console.log(error);
@@ -13,8 +22,7 @@ d3.csv("gameinfo.csv", function(error,csvdata){
     var score1 = csvdata[1].score;
 
     var teamData = [{team:team0, score:score0},{team:team1, score:score1}];
-    var dateData = [{year:csvdata[0].year, month:csvdata[0].month, day:csvdata[0].day}]
-
+    var dateData = [{year:csvdata[0].year, month:csvdata[0].month, day:csvdata[0].day}];
     var Team = d3.select("#game_title_field")
         .append("g")
         .attr("id", "team_info");
@@ -24,7 +32,6 @@ d3.csv("gameinfo.csv", function(error,csvdata){
     var Date = d3.select("#game_title_field")
         .append("g")
         .attr("id", "date_info");
-
     var svg_team = d3.select("#team_info")
         .selectAll("g")
         .data(teamData)
@@ -40,8 +47,8 @@ d3.csv("gameinfo.csv", function(error,csvdata){
         .attr("font-family", "华文行楷")
         .attr("font-size", "30")
         .attr("color","black")
+        .on("click", function(d,i){return setteam(i)})
         .text(function(d){return d.team});
-
     var svg_score = d3.select("#score_info")
         .selectAll("g")
         .data(teamData)
@@ -58,7 +65,6 @@ d3.csv("gameinfo.csv", function(error,csvdata){
         .attr("font-size", "40")
         .attr("color","black")
         .text(function(d){return d.score});
-
     var date = d3.select("#date_info")
         .selectAll("g")
         .data(dateData)
@@ -75,7 +81,73 @@ d3.csv("gameinfo.csv", function(error,csvdata){
         .attr("font-size", "20")
         .attr("color","black")
         .text(function(d){return "date : " + d.year + "-" + d.month + "-" + d.day});
-
 });
 
+//player info
+/**
+ * Created by WuJiang on 2017/7/8.
+ */
+
+var field1 = document.getElementById("player_svg_field").getBoundingClientRect();
+var field_w1 = field1.width, field_h1 = field1.height;
+
+function repaint_player_svg()
+{
+    // var p = body.select("circle");
+    // p.remove();
+    d3.csv("playerinfo.csv", function(error,csvdata){
+        if(error) console.log(error);
+        console.log(csvdata);
+
+        var Player_pos = d3.select("#player_svg_field")
+            .append("g")
+            .attr("id", "player_pos")
+            .attr("class", "fieldLines");
+        var Player_id = d3.select("#player_svg_field")
+            .append("g")
+            .attr("id", "player_id");
+        var svg_player_pos = d3.select("#player_pos")
+            .selectAll("g")
+            .data(csvdata)
+            .enter()
+            .append("circle")
+            .attr("class", "Player_pos")
+            .attr("id", function(d,i){return i})
+            .attr("cx", function(d)
+            {
+                console.log(d.team); console.log(teamchoose);
+                if(d.team == teamchoose && d.on_time < time && d.off_time > time) return d.y*field_w1/70;
+                else return -7
+            })
+            .attr("cy", function(d)
+            {
+                if(d.team == teamchoose && d.on_time < time && d.off_time > time) return d.x*field_h1/100;
+                else return -7
+            })
+            .attr("r", 7);
+        var svg_player_id = d3.select("#player_id")
+            .selectAll("g")
+            .data(csvdata)
+            .enter()
+            .append("text")
+            .attr("class", "Player_id")
+            .attr("id", function(d,i){return i})
+            .attr("x", function(d)
+            {
+                if(d.team == teamchoose && d.on_time < time && d.off_time > time) return d.y*field_w1/70;
+                else return -7
+            })
+            .attr("y", function(d)
+            {
+                if(d.team == teamchoose && d.on_time < time && d.off_time > time) return d.x*field_h1/100;
+                else return -7
+            })
+            .attr("text-anchor", "middle")
+            .attr("dominant-baseline", "middle")
+            .attr("font-family", "Arial")
+            .attr("font-size", 5)
+            .attr("color","red")
+            .text(function(d){return d.id});
+    });
+}
 
