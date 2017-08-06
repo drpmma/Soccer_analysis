ae73593027ce53c7c41f7b32f2c6f6b18358ed7e
zjuzf <3150102181@zju.edu.cn>
2017-08-07 00:14:47 +0800

    timeline

--- retimeline.js
+++ retimeline.js
@@ -1,7 +1,35 @@
 /**
  * Created by zf on 2017/7/27.
  */
-Sequence.prototype.draw_timeline = function()
-{
+timeline= function (svg,width,height) {
+    this.svg=svg;
+    this.width=width;
+    this.height=height;
 
+    this.g_timeline=svg.append("g")
+        .attr("id","timeline");
+    this.g_timeline.append("line")
+        .attr("x1",0.2*width)
+        .attr("y1",0.13*height)
+        .attr("x2",0.8*width)
+        .attr("y2",0.13*height)
+        .attr("stroke","black")
+        .attr("stroke-width","1.5px")
+    this.g_timeline.append("line")
+        .attr("x1",0.2*width)
+        .attr("y1",0.15*height)
+        .attr("x2",0.8*width)
+        .attr("y2",0.15*height)
+        .attr("stroke","black")
+        .attr("stroke-width","1.5px")
+    for(var i=0;i<7;i++) {
+        this.g_timeline.append("line")
+            .attr("x1", (0.2 + i * 0.1) * width)
+            .attr("y1", 0.13 * height)
+            .attr("x2", (0.2 + i * 0.1) * width)
+            .attr("y2", 0.15 * height)
+            .attr("stroke", "black")
+            .attr("stroke-width", "1px")
+    }
 }
+
