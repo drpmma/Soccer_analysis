2aceb35195debfacecceb9b08cd93fdcfb1889c2
zjuzf <3150102181@zju.edu.cn>
2017-08-07 08:47:11 +0800

    timeline

--- retimeline.js
+++ retimeline.js
@@ -6,6 +6,15 @@ timeline= function (svg,width,height) {
     this.width=width;
     this.height=height;
 
+    this.title=svg.append("rect")
+        .attr("id","teamname")
+        .attr("x",0.2*width)
+        .attr("y",0.02*height)
+        .attr("height",0.1*height)
+        .attr("width",0.6*width)
+        .attr("fill","white")
+        .attr("stroke","black")
+        .attr("stroke-width","0.5px")
     this.g_timeline=svg.append("g")
         .attr("id","timeline");
     this.g_timeline.append("line")
@@ -30,6 +39,34 @@ timeline= function (svg,width,height) {
             .attr("y2", 0.15 * height)
             .attr("stroke", "black")
             .attr("stroke-width", "1px")
+        this.g_timeline.append('text')
+            .attr("x",(0.2 + i * 0.1) * width)
+            .attr('y',0.17*height)
+            .attr('fill',"black")
+            .attr('font-size',"100%")
+            .attr('text-align',"center" )
+            .html(i*15);
+    }
+}
+
+matchinfo = function (svg,data,width,height) {
+    this.svg=svg;
+    this.data=data;
+    this.width=width;
+    this.height=height;
+
+    this.g_sequence=svg.append("g")
+        .attr("id","Sequence")
+    for(var i=0;i<data.length;i++)
+    {
+        var g=this.g_sequence.append("g")
+            .attr("id",function () {
+                return "g_sequence"+(i+1)
+            })
+        g.append("circle")
+            .attr("rx",function () {
+                
+            })
     }
 }
 
